9cc70b280fd94c3dcf8c141171b170a1066cd0cc
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-12-26 19:05:43 +0530

    feat: Hack for scaling

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include "Bitmap.h"
-#include "BitmapFileHeader.h"
-#include "BitmapInfoHeader.h"
 #include <iostream>
 
 using namespace std;
+using namespace fractal;
+int main() {
+  int const WIDTH = 800;
+  int const HEIGHT = 600;
+  Bitmap bitmap(WIDTH, HEIGHT);
+  double min = 1e6;
+  double max = -1e6;
+  // scaling hack
+  // the fractal values ranges from -1 to 1 on both axis
+  for (int y = 0; y < HEIGHT; y++) {
+    for (int x = 0; x < WIDTH; x++) {
+      double xFractal = (x - WIDTH / 2) * 2.0 / WIDTH;
+      double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
 
-int main() {}
+      if (xFractal < min) {
+        min = xFractal;
+      }
+      if (xFractal > max) {
+        max = xFractal;
+      }
+    }
+  }
+  cout << min << ", " << max << endl;
+  bitmap.write("test.bmp");
+  cout << "Complete";
+  return 0;
+}
