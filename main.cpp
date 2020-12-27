d08c1e524530d15fae13c8e52b5081f71faf31d6
adityakrmaurya <adityakrmaurya03@gmail.com>
2020-12-27 20:51:57 +0530

    feat: Histogram array to store iterations
    The Histogram array of size MAX_ITERATIONS + 1 is used to store the times
    the particular iteration occur in the variation of pixels the width and height.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Bitmap.h"
 #include "Mandelbrot.h"
 #include <iostream>
+#include <memory>
 
 using namespace std;
 using namespace fractal;
@@ -12,22 +13,33 @@ int main() {
   double max = -1e6;
   // scaling hack
   // the fractal values ranges from -1 to 1 on both axis
+  unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1]{0});
   for (int y = 0; y < HEIGHT; y++) {
     for (int x = 0; x < WIDTH; x++) {
-      double xFractal = (x - WIDTH / 2) * 2.0 / WIDTH;
+      double xFractal = (x - WIDTH / 2 - 100) * 2.0 / HEIGHT;
       double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
       int iterations = Mandelbrot::getIterations(xFractal, yFractal);
-      uint8_t red =
+      // increment the value of that iteration in the array
+      histogram[iterations]++;
+      uint8_t color =
           (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
-      bitmap.setPixel(x, y, red, red, red);
-      if (red < min) {
-        min = red;
+      color *= color * color;
+      bitmap.setPixel(x, y, 9, color, 9);
+      if (color < min) {
+        min = color;
       }
-      if (red > max) {
-        max = red;
+      if (color > max) {
+        max = color;
       }
     }
   }
+  cout << endl;
+  int count = 0;
+  for (int i = 0; i <= Mandelbrot::MAX_ITERATIONS; i++) {
+    cout << histogram[i] << " " << flush;
+    count += histogram[i];
+  }
+  cout << endl << count << "; " << WIDTH * HEIGHT << endl;
   cout << min << ", " << max << endl;
   bitmap.write("test.bmp");
   cout << "Complete";
