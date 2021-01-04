4e4a7738b7fd34cbc57b8486f957b055c54aee1f
adityakrmaurya <adityakrmaurya03@gmail.com>
2021-01-05 01:26:06 +0530

    style: change file names

--- ZoomList.cpp
+++
@@ -1,9 +0,0 @@
-#include "ZoomList.h"
-
-namespace fractal {
-  ZoomList::ZoomList(int width, int height): m_width(width), m_height(height) {}
-  void ZoomList::add(const Zoom& zoom) { zooms.push_back(zoom); }
-  std::pair<double, double> ZoomList::doZoom(int x, int y) {
-    return std::pair<double, double>(0, 0);
-  }
-}
