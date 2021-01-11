5189ed21e06501474393fd71e4b6d400e9213a07
adityakrmaurya <adityakrmaurya03@gmail.com>
2021-01-11 08:59:51 +0530

    fix: removed unused variable

--- bitmap.cc
+++ bitmap.cc
@@ -4,16 +4,13 @@
 
 #include "bitmap_file_header.h"
 #include "bitmap_info_header.h"
-
-using namespace std;
-
 namespace fractal {
 Bitmap::Bitmap(int width, int height)
     : width_(width),
       height_(height),
       p_pixels_(new uint8_t[width * height * 3]{}) {}
 
-bool Bitmap::Write(std::string filename) {
+bool Bitmap::Write(std::string const &filename) {
   BitmapFileHeader file_header;
   BitmapInfoHeader info_header;
   file_header.file_size = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) +
@@ -26,20 +23,14 @@ bool Bitmap::Write(std::string filename) {
   if (!file) {
     return false;
   }
-  file.write((char *)&file_header, sizeof(file_header));
-  file.write((char *)&info_header, sizeof(info_header));
-  file.write((char *)p_pixels_.get(), width_ * height_ * 3);
-
+  file.write(reinterpret_cast<char *>(&file_header), sizeof(file_header));
+  file.write(reinterpret_cast<char *>(&info_header), sizeof(info_header));
+  file.write(reinterpret_cast<char *>(p_pixels_.get()), width_ * height_ * 3);
   file.close();
-  if (!file) {
-    return false;
-  }
   return true;
 }
 // x and y are like coordinate plane points
 void Bitmap::SetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
-  int c;
-
   uint8_t *pPixel = p_pixels_.get();
   pPixel += (3 * y) * width_ + (3 * x);
   pPixel[0] = blue;
